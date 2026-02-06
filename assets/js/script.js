6f038e0c573b63483d670f1e82a96c4535c3692c
josdias <jdmota520@gmail.com>
2026-02-06 00:07:26 -0300

    refactor: remove comentários desnecessários e código comentado para melhorar a legibilidade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,19 @@
-// Seleciona os elementos
 const loginArea = document.getElementById("loginArea");
 const registerArea = document.getElementById("registerArea");
 
-// Define o estado inicial (Login visível)
+
 window.onload = () => {
   loginArea.classList.add("active");
   registerArea.classList.remove("active");
-  // Move o registro para a direita (preparado para entrar)
+  
   registerArea.style.transform = "translate(50%, -50%)";
 };
 
 function openRegister() {
-  // Login sai para a esquerda
   loginArea.style.transform = "translate(-150%, -50%)";
   loginArea.style.opacity = "0";
   loginArea.style.pointerEvents = "none";
 
-  // Registro entra vindo da direita
   registerArea.style.transform = "translate(-50%, -50%)";
   registerArea.style.opacity = "1";
   registerArea.style.pointerEvents = "all";
@@ -24,12 +21,10 @@ function openRegister() {
 }
 
 function openLogin() {
-  // Registro volta para a direita
   registerArea.style.transform = "translate(50%, -50%)";
   registerArea.style.opacity = "0";
   registerArea.style.pointerEvents = "none";
 
-  // Login volta para o centro
   loginArea.style.transform = "translate(-50%, -50%)";
   loginArea.style.opacity = "1";
   loginArea.style.pointerEvents = "all";
